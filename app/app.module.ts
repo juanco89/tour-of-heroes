5c5fd3c141739e53141651d60ebf321d37418daf
Juan C. Orozco <juanco89@gmail.com>
2017-01-15 11:53:54 -0500

    Se implementa la última parte del tutorial tour of heroes.
    Hero Search Component & Observables.

--- app/app.module.ts
+++ app/app.module.ts
@@ -12,8 +12,10 @@ import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import './rxjs-extensions';
 
 @NgModule({
   imports: [ 
@@ -27,7 +29,8 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    DashboardComponent 
+    DashboardComponent,
+    HeroSearchComponent
   ],
   providers: [
      HeroService
